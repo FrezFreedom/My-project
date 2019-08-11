f236697ba8ad7be502e52910dde8c2daa6c8b9c9
Faraz Azadi <frez1374@gmail.com>
2019-08-11 06:14:48 -0700

    Add dijkstra function for finding maximum bottleneck in paths between two vertices

--- project/SubsidiaryFunctions.cpp
+++ project/SubsidiaryFunctions.cpp
@@ -25,3 +25,33 @@ MatrixXd normalize_matrix_special(MatrixXd M, int n, double a, double b){
 	}
 	return M;
 }
+
+double dijkstra(double ** graph, int size, int src, int dst){
+	int srcc = src, dstt = dst;
+	unordered_set<int> check;
+	unordered_map<int, int> back_node;
+
+	priority_queue< pair<double, double> > q;
+	q.push(make_pair(1e9, src));
+
+	while (!q.empty()){
+		pair<double, int> help = q.top();
+		q.pop();
+		
+		if (help.second == dst)return help.first;
+		if (check.find(help.second) == check.end()){
+			check.insert(help.second);
+			//size = NN
+			for (int i = 0; i < size; ++i)
+			{
+				if (i != help.second && check.find(i) == check.end() && graph[help.second][i])
+				{
+					q.push(make_pair(min(graph[help.second][i], help.first), i));
+					back_node[i] = help.second;
+				}
+			}
+		}
+
+	}
+	return 0;
+}
--- project/SubsidiaryFunctions.h
+++ project/SubsidiaryFunctions.h
@@ -13,3 +13,5 @@ using namespace std;
 MatrixXd normalize_matrix(MatrixXd M, int n, double MAX);
 
 MatrixXd normalize_matrix_special(MatrixXd M, int n, double a, double b);
+
+double dijkstra(double ** graph, int size, int src, int dst);
